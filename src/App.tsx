6ae94899cf4f758f21b0eb7e57a5f261b50b23bf
QWE123-QWE321 <yjyjjy5201314@gmail.com>
2026-03-11 15:52:47 +0800

    fix: 移除未使用的React导入

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 
-
-const App = () => {
+export default function App() {
   return (
-    <div className="min-h-screen bg-slate-300 flex items-center justify-center font-outfit">
-     <div>111</div>
+    <div>
+      
     </div>
-  );
-};
-
-export default App;
+  )
+}
